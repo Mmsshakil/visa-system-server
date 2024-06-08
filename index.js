456a2d5a8c56b441605ba70e8b8af3e31e809260
Mmsshakil <softhandshakil@gmail.com>
2024-06-09 03:09:27 +0600

    user registration all data added to the server

--- index.js
+++ index.js
@@ -29,6 +29,41 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const usersCollection = client.db('visaDB').collection('users');
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user does not exists
+            // 1.email unique, 2. upsert ,3.simple check in and many ore ways
+
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null });
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
