cfdac9efe2fc35ddce0a2a3380c9684ffc446a03
Mmsshakil <softhandshakil@gmail.com>
2024-06-25 03:25:42 +0600

    verify token and verify admin middleware added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
@@ -35,6 +36,53 @@ async function run() {
         const jobsCollection = client.db('visaDB').collection('jobs');
         const countriesCollection = client.db('visaDB').collection('countries');
 
+        // jwt realted api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' });
+            res.send({ token });
+        })
+
+        // middlewares
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'forbidden access' });
+            }
+            const token = req.headers.authorization.split(' ')[1];
+            // if (!token) {
+            //     return res.status(401).send({ message: 'forbidden access' });
+            // }
+
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'forbidden access' });
+                }
+                req.decoded = decoded;
+                next();
+            })
+
+            // next();
+        }
+
+
+        // verify admin middleware
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await usersCollection.find().query();
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.send(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+
+
+
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             // insert email if user does not exists
@@ -52,12 +100,27 @@ async function run() {
 
 
         // show all users data
-        app.get('/allusers', async (req, res) => {
+        app.get('/allusers', verifyToken, async (req, res) => {
+            // console.log(req.headers);
             const cursor = usersCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         })
 
+        // admin check api
+        app.get('/user/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+            if (email !== req.decoded.email) {
+                return res.send(403).send({ message: 'unauthorized access' })
+            }
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
 
 
         // get users of specific user's email
